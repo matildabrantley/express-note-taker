080124c6a0e28bee4527b4dbbd2ecef9da6d0f99
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-18 19:44:53 -0400

    api routes all working, db.json reading/writing

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const notesData = require('../data/db.json');
+const notesData = require('../public/db.json');
 const fs = require('fs');
-
 
 //All Routing
 module.exports = (app) => {
@@ -10,14 +9,35 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../public/index.html'));
+  // });
 
   //API Routing
-  app.get('/api/notes', (req, res) => res.json(notesData));
+   app.get('/api/notes', (req, res) => res.json(notesData));
+   
+  // app.get('/api/notes', (req, res) => {
+  //   let filePath = path.join(__dirname, '../public/db.json');
+  //   fs.readFile(filePath, 'utf8', (err, data) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     res.send(JSON.parse(data));
+  //   });
+  //   //res.json(filePath);
+  // });
 
   app.post('/api/notes', (req, res) => {
-    fs.writeFileSync('db.json', req.body);
+    let filePath = path.join(__dirname, '../public/db.json');
+    notesData.push(req.body);
+    fs.writeFile(filePath, JSON.stringify(notesData), function() {
+      res.end();
+    });
   });
+  // app.post('/api/notes', (req, res) => {
+  //   let filePath = path.join(__dirname, '../public/db.json');
+  //   fs.appendFile(filePath, JSON.stringify(req.body), function() {
+  //     res.end();
+  //   });
+  // });
 };
