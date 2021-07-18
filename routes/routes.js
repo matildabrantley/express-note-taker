5105994f61b40334ff77640121e9261bad3e1713
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-17 22:20:55 -0400

    allow static files (css), added textareas for input

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ const path = require('path');
 //HTML Routing
 module.exports = (app) => {
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 };
