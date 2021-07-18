95291a2b5d38f9e6a7a6591c1edc532ac7cd7e45
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-17 23:55:47 -0400

    api routes added, notes appended to bootstrap style list

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const notesData = require('../data/db.json');
+const fs = require('fs');
 
-//HTML Routing
+
+//All Routing
 module.exports = (app) => {
+  //HTML Routing
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  //API Routing
+  app.get('/api/notes', (req, res) => res.json(notesData));
+
+  app.post('/api/notes', (req, res) => {
+    fs.writeFileSync('db.json', req.body);
   });
 };
